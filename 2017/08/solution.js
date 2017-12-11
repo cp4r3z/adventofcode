30f74158e069d26a569f794a8c13e7e4888ce3a0
cp4r3z <cp4r3z@gmail.com>
2017-12-11 00:11:02 +0000

    Should be working. Each suspect.

--- 2017/08/solution.js
+++ 2017/08/solution.js
@@ -21,17 +21,73 @@ let instructions = [];
 for (let line of lines) {
     const re = /(\w+) ([dec,inc]+) ([-\d]+) if (\w+) ([><!=]+) ([-\d]+)/;
     const parsedLine = line.match(re);
-    console.log(parsedLine);
+    //console.log(parsedLine);
     const instruction = {};
-    
+
     instruction.target = parsedLine[1];
     instruction.operation = parsedLine[2];
-    instruction.amount= parseInt(parsedLine[3]);
+    instruction.amount = parseInt(parsedLine[3]);
     instruction.compareTarget = parsedLine[4];
     instruction.comparator = parsedLine[5];
     instruction.compareAmount = parseInt(parsedLine[6]);
-    
+
     instructions.push(instruction);
 }
 
+let registers = {};
+
+//first handle the conditional
+
+_.each(instructions, (instruction) => {
+    if (registers[instruction.compareTarget]==undefined){
+            console.log('compareTarget created')
+            registers[instruction.compareTarget]=0;
+        }
+    switch (instruction.comparator) {
+        case '>':
+            if (instruction.compareTarget > instruction.compareAmount) followInstruction(instruction);
+            
+            break;
+        case '>=':
+            if (instruction.compareTarget >= instruction.compareAmount) followInstruction(instruction);
+            break;
+        case '<':
+            if (instruction.compareTarget < instruction.compareAmount) followInstruction(instruction);
+            break;
+        case '<=':
+            if (instruction.compareTarget <= instruction.compareAmount) followInstruction(instruction);
+            break;
+        case '==':
+            if (instruction.compareTarget == instruction.compareAmount) followInstruction(instruction);
+            break;
+        case '!=':
+            if (instruction.compareTarget != instruction.compareAmount) followInstruction(instruction);
+            break;
+        default:
+            // code
+            console.log(instruction.comparator + 'not found!');
+    }
+    
+    function followInstruction(instruction){
+        if (registers[instruction.target]==undefined){
+            registers[instruction.target]=0;
+        }
+        switch (instruction.operation) {
+            case 'inc':
+                registers[instruction.target]+=instruction.amount;
+                break;
+            case 'dec':
+                registers[instruction.target]-=instruction.amount;
+                break;
+            default:
+                // code
+        }    
+    
+    }
+})
+
+console.log(registers);
+
+const max = _.max(_.values(registers));
+console.log(max);
 console.log(instructions);
