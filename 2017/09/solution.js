1726b0bb7e9053bedd0b8598e16e6d4a7b5ddfba
cp4r3z <cp4r3z@gmail.com>
2017-12-13 03:23:59 +0000

    Day 9 Solution

--- 2017/09/solution.js
+++ 2017/09/solution.js
@@ -2,7 +2,7 @@
  * https://adventofcode.com/2017/day/9
  */
 
-const input = 'input0.txt';
+const input = 'input.txt';
 
 const fs = require('fs'),
     _ = require('underscore');
@@ -17,7 +17,7 @@ while (cleanup) {
     cleanup = false;
     for (let i = 0; i < file.length; i++) {
         if (file.substr(i, 1) === '!') {
-            console.log(`Found a ! @ position ${i}.`);
+            //console.log(`Found a ! @ position ${i}.`);
             cleanup = true;
             file = file.slice(0, i) + file.slice(i + 2);
             // poor man's for loop break
@@ -25,10 +25,11 @@ while (cleanup) {
         }
     }
 }
-console.log('! symbols removed');
-console.log(file);
+//console.log('! symbols removed');
+//console.log(file);
 
 // remove garbage
+let totalGarbage =0;
 cleanup = true;
 while (cleanup) {
     cleanup = false;
@@ -36,42 +37,64 @@ while (cleanup) {
     let garbageEnd;
     for (let i = 0; i < file.length; i++) {
         if (garbageStart === undefined && file.substr(i, 1) === '<') {
-            console.log(`Found a < @ position ${i}.`);
+            //console.log(`Found a < @ position ${i}.`);
             garbageStart = i;
         }
         else if (garbageStart !== undefined && file.substr(i, 1) === '>') {
-            console.log(`Found a > @ position ${i}.`);
+            //console.log(`Found a > @ position ${i}.`);
             garbageEnd = i;
             file = file.slice(0, garbageStart) + file.slice(garbageEnd + 1);
+            totalGarbage += garbageEnd-garbageStart-1;
             cleanup = true;
             // poor man's for loop break
             i = file.length;
         }
     }
 }
-console.log('garbage removed')
-console.log(file);
-
+//console.log('garbage removed')
+//console.log(file);
+console.log(`Total Garbage: ${totalGarbage}`)
 // count groups
 let count = 1;
 const re = /\{(.+)\}/
 
-function addToCount(lev, str) {
+function getInnerObjs(str) {
+    let pos = {};
+    pos.start = 0;
+    pos.end = 0;
+    let depth = 0;
+    let innerObjs = [];
 
-    const inner = str.substr(1, str.length - 2);
-    console.log(inner);
-    if (inner.length > 0) {
-        const groups = inner.split(',');
-        if (groups.length > 0) {
-            for (let group of groups) {
-                console.log(group);
-                count += lev;
-                addToCount(lev + 1, group);
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] == "}") {
+            if (depth == 2) {
+                pos.end = i;
+                innerObjs.push(str.substr(pos.start, (pos.end - pos.start + 1)));
             }
+            depth--;
+        }
+        if (str[i] == "{") {
+            if (depth == 1) {
+                pos.start = i;
+            }
+            depth++;
+        }
+
+    }
+    return innerObjs;
+}
+
+function addToCount(lev, str) {
+    const groups = getInnerObjs(str);
+    if (groups.length > 0) {
+        for (let group of groups) {
+            //console.log(group);
+            count += lev;
+            addToCount(lev + 1, group);
         }
     }
 }
 
 let level = 2;
 addToCount(level, file);
-console.log(count)
+console.log(`Count: ${count}`);
