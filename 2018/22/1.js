8561bd8773e81c7e7b0bf16be6c5cdfa547d88d5
cp4r3z <cp4r3z@gmail.com>
2019-01-19 03:15:53 +0000

    Day 22 Solution 2

--- 2018/22/1.js
+++ 2018/22/1.js
@@ -4,13 +4,13 @@
 
 const _ = require('underscore'); // Not used?
 
-// const depth = 11109;
-// const target = { x: 9, y: 731 };
+const depth = 11109;
+const target = { x: 9, y: 731 };
 
-const depth = 510;
-const target = { x: 10, y: 10 };
+// const depth = 510;
+// const target = { x: 10, y: 10 };
 
-const caveBleed = 5; // extra cave to draw in case you need to move around an obstacle
+const caveBleed = 40; // extra cave to draw in case you need to move around an obstacle
 let cave = Array(target.y + 1 + caveBleed).fill(0).map(y => (Array(target.x + 1 + caveBleed).fill(0)));
 
 for (var y = 0; y < cave.length; y++) {
@@ -21,64 +21,67 @@ for (var y = 0; y < cave.length; y++) {
 }
 
 //logCave();
+console.log(`Solution 1: Risk = ${getRisk()}`);
 
-//console.log(`Solution 1: Risk = ${getRisk()}`);
-
-//now... maybe a new object...
 // key: x0y0, x10y10 etc...
 // props: x, y, type?, array of surrounding tiles
 let caveObj = {};
+
+// Start with worst case scenario
+let bestTime = (target.x + target.y) * 8;
 
 for (var y = 0; y < cave.length; y++) {
     for (var x = 0; x < cave[0].length; x++) {
         const key = `x${x}y${y}`;
         let adj = [];
         if (x > 0) adj.push(`x${x-1}y${y}`); // left
-        if (y > 0) adj.push(`x${x}y${y-1}`); // up?
-        if (y < cave.length - 1) adj.push(`x${x}y${y+1}`); // down?
+        if (y > 0) adj.push(`x${x}y${y-1}`); // up
+        if (y < cave.length - 1) adj.push(`x${x}y${y+1}`); // down
         if (x < cave[0].length - 1) adj.push(`x${x+1}y${y}`); //right
         let isTarget = false;
         if (x == target.x && y == target.y) isTarget = true;
         const type = cave[y][x].type;
-        const props = { x, y, adj, type, isTarget };
+        const bestTimes = {
+            'C': (x + y) * 8,
+            'N': (x + y) * 8,
+            'T': (x + y) * 8
+        };
+        const props = { x, y, adj, type, isTarget, bestTimes };
 
         caveObj[key] = props;
     }
 }
 
 // equip can be T=torch, C=climbing gear, N=neither
-
-const testMove = getOptions('x0y0', 'T');
-
-// Start with worst case scenario
-let bestTime = (target.x + target.y) * 8;
-
 let routes = [{
     distanceToTarget: target.x + target.y,
     route: ['x0y0'],
+    routeTimes: [0],
     equip: 'T',
+    equips: ['T'],
     time: 0,
     atTarget: false // todo: maybe we don't actually have to track this. Just delete em.
 }];
 
-let foundSolution = false;
+const breadth = 4; // To adjust breadth vs depth searching
+
+// 2 - 5 seems to be the ideal. Larger breadth just increases time.
+
+//console.log(new Date());
 let start = true;
-
-const breadth = 40; // To adjust breadth vs depth searching
-
-while (start || (!foundSolution && routes.length > 1)) {
+while (start || routes.length > 0) {
+    //console.log(bestTime + ' ' + routes.length);
     if (start) start = false;
-    // generate a bunch of routes
-    // start by sorting by distanceToTarget in ASCENDING order
-    routes.sort((ra, rb) => rb.distanceToTarget - ra.distanceToTarget);
-    // get the breadth of the closest.
+
+    // start by sorting by time in descending order
+    routes.sort((ra, rb) => rb.time - ra.time);
+
+    // get the breadth of the closest
     const closest = _.last(routes, breadth);
+
     let newRoutes = [];
+    
     // now add all the adjacent routes
-    // TODO: NO NO NO, this needs to be done in a for loop because I need the index
-    //for (let rIndex = routes.length - breadth; rIndex < routes.length; rIndex++) {
-    // const r = routes[rIndex];
-    //}
     closest.forEach(r => {
         //get the last of the keys
         const lastKey = r.route[r.route.length - 1];
@@ -88,53 +91,66 @@ while (start || (!foundSolution && routes.length > 1)) {
             if (r.route.indexOf(o.key) < 0) {
                 const route = r.route.concat([o.key]);
                 const time = r.time + o.time;
+                const routeTimes = r.routeTimes.concat([time]);
                 const equip = o.equip;
+                const equips = r.equips.concat([equip]);
                 const distanceToTarget = Math.abs(target.x - caveObj[o.key].x) + Math.abs(target.y - caveObj[o.key].y);
-                //console.log(distanceToTarget);
-                const atTarget = distanceToTarget == 0; // todo: maybe this isn't necessary. just use distance to target
+                const atTarget = distanceToTarget == 0;
                 // Push new route if we're not there yet and we're under time.
-                if (!atTarget && time < bestTime) {
-                    newRoutes.push({
-                        atTarget,
-                        distanceToTarget,
-                        equip,
-                        route,
-                        time
-                    });
+                if (time < caveObj[o.key].bestTimes[o.equip]) {
+                    caveObj[o.key].bestTimes[o.equip] = time;
+                    if (!atTarget && (time + distanceToTarget) < bestTime) {
+                        newRoutes.push({
+                            atTarget,
+                            distanceToTarget,
+                            equip,
+                            equips,
+                            routeTimes,
+                            route,
+                            time
+                        });
+                    }
                 }
+
                 // If we're at the target, test to see if we beat our time.
                 if (atTarget && time < bestTime) {
                     bestTime = time;
-                    console.log(`New Best Time! ${bestTime}`);
+                    //console.log(`New Best Time! ${bestTime}`);
                 }
             }
 
-        })
-        // now delete the original route and add the new options. or maybe delete closest out of this loop...
-        // maybe mark for deletion...
+        });
+        // now delete the original route and add the new options
         r.deleteMe = true;
         routes = routes.concat(newRoutes);
-        //debugger;
-    })
+    });
     // ALSO, filter out all routes with higher times than the best
     routes = _.filter(routes, r => !r.deleteMe && r.time < bestTime);
-    //debugger;
 
-
-    //TODO: Delete all routes that are longer than bestTime
-
+    // The following was a good idea, but just took time
+    // routes = _.filter(routes, r => {
+    //     let ok = true;
+    //     for (var rIndex = 0; rIndex < r.route.length; rIndex++) {
+    //         const key = r.route[rIndex];
+    //         if (key != 'x0y0') {
+    //             if (caveObj[key].bestTimes[r.equips[rIndex]] < r.routeTimes[rIndex]) {
+    //                 ok = false;
+    //             }
+    //         }
+    //     }
+    //     return ok;
+    // });
 }
-
-//moving from tile to tile...
+//console.log(new Date())
+console.log(`Solution 2: Best Time = ${bestTime}`);
+//1008 is the answer
 
 // input = caveObj key 1, caveObj key 2, current equip
 // output = array of possible keys, equips and times
-
 function getOptions(_keyFrom, _equip) {
     let options = [];
     caveObj[_keyFrom].adj.forEach(key => {
-        if (key.x == target.x && key.y == target.y) {
-            foundSolution = true;
+        if (caveObj[key].x == target.x && caveObj[key].y == target.y) {
             options.push({ key, time: _equip == 'T' ? 1 : 8, equip: 'T' });
         }
         else if (caveObj[key].type == 0) {
