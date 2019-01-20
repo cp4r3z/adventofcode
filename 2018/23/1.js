a7dac3c9095d410dccb3dc06fdfb3bb96bed0d4a
cp4r3z <cp4r3z@gmail.com>
2019-01-20 14:39:38 +0000

    Day 23 solution 2 wip

--- 2018/23/1.js
+++ 2018/23/1.js
@@ -34,5 +34,113 @@ function getManhattenDistance(coorA, coorB) {
     return coorA.reduce((distance, dimA, i) => distance += Math.abs(coorB[i] - dimA), 0);
 }
 
+// Find min/max of all?
+
+let min = {};
+let max = {};
+
+points.forEach(p => {
+    if (!min.x || p.coor[0] < min.x) min.x = p.coor[0];
+    if (!min.y || p.coor[1] < min.y) min.y = p.coor[1];
+    if (!min.z || p.coor[2] < min.z) min.z = p.coor[2];
+    if (!max.x || p.coor[0] > max.x) max.x = p.coor[0];
+    if (!max.y || p.coor[1] > max.y) max.y = p.coor[1];
+    if (!max.z || p.coor[2] > max.z) max.z = p.coor[2];
+});
+
+let allPoints = [];
+
+// Yeah, this is crazy sauce:
+/*
+for (var ix = min.x; ix <= max.x; ix++) {
+    for (var iy = min.y; iy <= max.y; iy++) {
+        for (var iz = min.z; iz <= max.z; iz++) {
+            allPoints.push({
+                x: ix,
+                y: iy,
+                z: iz
+            })
+        }
+    }
+}
+*/
+
+let octs = createOcts(min, max);
+let maxOcts = [];
+
+// Assume min and max both have x, y and z params
+function createOcts(_min, _max) {
+    let _mid = {};
+    _mid.x = Math.round((_max.x + _min.x) / 2);
+    _mid.y = Math.round((_max.y + _min.y) / 2);
+    _mid.z = Math.round((_max.z + _min.z) / 2);
+
+    let octs = Array(8).fill(0).map(o => {
+        return {
+            min: { x: 0, y: 0, z: 0 },
+            max: { x: 0, y: 0, z: 0 }
+        };
+    });
+    //let out = Array(size.ymax - size.ymin + 1).fill(0).map(x => (Array(size.xmax - size.xmin + 1).fill(".")));
+
+    octs[0].min.x = _min.x;
+    octs[0].min.y = _min.y;
+    octs[0].min.z = _min.z;
+    octs[0].max.x = _mid.x;
+    octs[0].max.y = _mid.y;
+    octs[0].max.z = _mid.z;
+
+    octs[1].min.x = _mid.x + 1; //
+    octs[1].min.y = _min.y;
+    octs[1].min.z = _min.z;
+    octs[1].max.x = _max.x; //
+    octs[1].max.y = _mid.y;
+    octs[1].max.z = _mid.z;
+
+    octs[2].min.x = _min.x;
+    octs[2].min.y = _mid.y + 1; //
+    octs[2].min.z = _min.z;
+    octs[2].max.x = _mid.x;
+    octs[2].max.y = _max.y; //
+    octs[2].max.z = _mid.z;
+
+    octs[3].min.x = _min.x;
+    octs[3].min.y = _min.y;
+    octs[3].min.z = _mid.z + 1; //
+    octs[3].max.x = _mid.x;
+    octs[3].max.y = _mid.y;
+    octs[3].max.z = _max.z; //
+
+    octs[4].min.x = _mid.x + 1; //
+    octs[4].min.y = _mid.y + 1; //
+    octs[4].min.z = _min.z;
+    octs[4].max.x = _max.x; //
+    octs[4].max.y = _max.y; //
+    octs[4].max.z = _mid.z;
+
+    octs[5].min.x = _mid.x + 1; //
+    octs[5].min.y = _min.y;
+    octs[5].min.z = _mid.z + 1; //
+    octs[5].max.x = _max.x; //
+    octs[5].max.y = _mid.y;
+    octs[5].max.z = _max.z; //
+
+    octs[6].min.x = _min.x;
+    octs[6].min.y = _mid.y + 1; //
+    octs[6].min.z = _mid.z + 1; //
+    octs[6].max.x = _mid.x;
+    octs[6].max.y = _max.y; //
+    octs[6].max.z = _max.z; //
+
+    octs[7].min.x = _mid.x + 1; //
+    octs[7].min.y = _mid.y + 1; //
+    octs[7].min.z = _mid.z + 1; //
+    octs[7].max.x = _max.x; //
+    octs[7].max.y = _max.y; //
+    octs[7].max.z = _max.z; //
+
+    return octs;
+}
+
 // End Process (gracefully)
 process.exit(0);
