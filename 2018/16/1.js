f2327edf70cc9b59a62312cd331969f795ad65b5
cp4r3z <cp4r3z@gmail.com>
2019-01-24 01:24:29 +0000

    Day 16 wip

---
+++ 2018/16/1.js
@@ -0,0 +1,160 @@
+/**
+ * https://adventofcode.com/2018/day16
+ */
+
+const _ = require('underscore');
+
+// Read input into simple array
+const arrInput = require('fs').readFileSync('input1.test.txt', 'utf8').split('\n\n');
+
+// Step 0 - Map Function - Use Regular Expression to parse each row
+
+const re = /Before: \[(\d+), (\d+), (\d+), (\d+)\]\n(\d+) (\d+) (\d+) (\d+)\nAfter:  \[(\d+), (\d+), (\d+), (\d+)\]/m;
+const step0 = row => re.exec(row);
+const step1 = row => {
+    return {
+        before: [~~row[1], ~~row[2], ~~row[3], ~~row[4]],
+        instruction: {
+            opcode: ~~row[5],
+            a: ~~row[6],
+            b: ~~row[7],
+            c: ~~row[8]
+        },
+        after: [~~row[9], ~~row[10], ~~row[11], ~~row[12]]
+    }
+};
+
+const samples = arrInput
+    .map(step0)
+    .map(step1);
+
+let registers = [0, 0, 0, 0];
+
+// const testIn = {
+//     opcode: 'addi',
+//     a: 0,
+//     b: 7,
+//     c: 3
+// };
+
+// op(testIn);
+
+const opcodes = [
+    'addr',
+    'addi',
+    'mulr',
+    'muli',
+    'banr',
+    'bani',
+    'borr',
+    'bori',
+    'setr',
+    'seti',
+    'gtir',
+    'gtri',
+    'gtrr',
+    'eqir',
+    'eqri',
+    'eqrr'
+];
+
+let opIndices = Array(16).fill(0).map(r => []);
+
+// for each sample, test against all possible opcodes, and evaluate if the after state is valid
+const validCodes = samples.map(s => {
+    opcodes.forEach(o => {
+        const before = s.before.slice(0);
+        const after = s.after.slice(0);
+        let instruction = s.instruction;
+        instruction.opcode = o;
+        const testAfter = op(instruction, before);
+        if (_.isEqual(testAfter, after)) {
+            //console.log('hi');
+            opIndices[instruction.opcode].push(o);
+        }
+    });
+});
+
+// instruction (or _in) take an object, containing:
+// opcode, a, b, c
+function op(_in, _r) {
+    const opcode = _in.opcode;
+    const a = _in.a;
+    const b = _in.b;
+    const c = _in.c;
+    let r = _r.slice(0);
+
+    switch (opcode) {
+        case 'addr':
+            // (add register) stores into register C the result of adding register A and register B
+            r[c] = r[a] + r[b];
+            break;
+        case 'addi':
+            // (add immediate) stores into register C the result of adding register A and value B
+            r[c] = r[a] + b;
+            break;
+        case 'mulr':
+            // (multiply register) stores into register C the result of multiplying register A and register B
+            r[c] = r[a] * r[b];
+            break;
+        case 'muli':
+            // (multiply immediate) stores into register C the result of multiplying register A and value B
+            r[c] = r[a] * b;
+            break;
+        case 'banr':
+            // (bitwise AND register) stores into register C the result of the bitwise AND of register A and register B.
+            r[c] = r[a] & r[b];
+            break;
+        case 'bani':
+            // (bitwise AND immediate) stores into register C the result of the bitwise AND of register A and value B
+            r[c] = r[a] & b;
+            break;
+        case 'borr':
+            // (bitwise OR register) stores into register C the result of the bitwise OR of register A and register B
+            r[c] = r[a] | r[b];
+            break;
+        case 'bori':
+            // (add immediate) stores into register C the result of adding register A and value B
+            r[c] = r[a] | b;
+            break;
+        case 'setr':
+            // (set register) copies the contents of register A into register C. (Input B is ignored.)
+            r[c] = r[a];
+            break;
+        case 'seti':
+            // (set immediate) stores value A into register C. (Input B is ignored.)
+            r[c] = a;
+            break;
+        case 'gtir':
+            // (greater-than immediate/register) sets register C to 1 if value A is greater than register B. Otherwise, register C is set to 0.
+            r[c] = a > r[b] ? 1 : 0;
+            break;
+        case 'gtri':
+            // (greater-than register/immediate) sets register C to 1 if register A is greater than value B. Otherwise, register C is set to 0.
+            r[c] = r[a] > b ? 1 : 0;
+            break;
+        case 'gtrr':
+            // (greater-than register/register) sets register C to 1 if register A is greater than register B. Otherwise, register C is set to 0.
+            r[c] = r[a] > r[b] ? 1 : 0;
+            break;
+        case 'eqir':
+            // (equal immediate/register) sets register C to 1 if value A is equal to register B. Otherwise, register C is set to 0.
+            r[c] = a == r[b] ? 1 : 0;
+            break;
+        case 'eqri':
+            // (equal register/immediate) sets register C to 1 if register A is equal to value B. Otherwise, register C is set to 0.
+            r[c] = r[a] == b ? 1 : 0;
+            break;
+        case 'eqrr':
+            // (equal register/register) sets register C to 1 if register A is equal to register B. Otherwise, register C is set to 0.
+            r[c] = r[a] == r[b] ? 1 : 0;
+            break;
+        default:
+            // should not happen
+            console.log('Operation Not Found.');
+    }
+    return r;
+}
+
+// End Process (gracefully)
+process.exit(0);
