99fe4eecf945bd31db67567722951fb093117602
cp4r3z <cp4r3z@gmail.com>
2019-02-16 12:53:40 -0600

    Day 21 Solutions

--- 2018/21/1.js
+++ 2018/21/1.js
@@ -4,11 +4,12 @@
 
 const _ = require('underscore');
 
+// Instruction Pointer
 // #ip 4
 const ip = 4;
 
 // Read input into simple array
-const arrInput = require('fs').readFileSync('input.txt', 'utf8').split('\n');
+const arrInput = require('fs').readFileSync('2018/21/input.txt', 'utf8').split('\n');
 
 const re = /(\w+) (\d+) (\d+) (\d+)/;
 const step0 = row => re.exec(row);
@@ -26,213 +27,44 @@ const program = arrInput
     .map(step0)
     .map(step1);
 
-//1361411
-//14514182
-// for (var i = 14514182; i < 14515182; i++) {
-//     const halted = run_program(80000+i, i);
-//     if (halted) {
-//         console.log(`halted: ${halted}`);
-//     }
-// }
-//let bip28 = [{ loop: 0, reg: 1105013081417 }, { loop: 0, reg: 1104296363893 }, { loop: 0, reg: 1101653155003 }, { loop: 0, reg: 1092633163479 }, { loop: 0, reg: 713604125745 }, { loop: 0, reg: 713589298470 }];
-let bip28 = [];
-let bip8 = [];
-let bipgrr = [];
-//const max = 17059512;
-const max = 407059512;
-// look at  172860175... reg = 53
-//const max = 7059512;
-// largest max = 16735068
-let largestReg5 = 0;
-// 1101653155003
-// 1105013081417
-// largestReg5=713589298470
-// largestReg5=713604125745
-// largestReg5=1092633163479
-// largestReg5=1101653155003
-// largestReg5=1104296363893
-// largestReg5=1105013081417
+let arrInstruction28 = []; // Array which holds all the registers[5] values found when instruction is 28
 
-//found this one... 14590535
-// and this one...14656071
-//genReg();
+const maxOps = 1e10; // Set some limit on the number of operations.
+run_program(maxOps, 0);
+
+console.log(`Part 1 Solution: ${arrInstruction28[0]}`);
+console.log(`Part 1 Solution: ${arrInstruction28[arrInstruction28.length-1]}`);
+
 // 202209 (first answer)
-run_program(max, 202209);
-//run_program(max, 0);
-// WRONG: 15298415 halts after ~ 172860175 loops
-//16738346
-//run_program(max, 0);
-
-
-//const tebip = _.intersection(bip28, bipgrr);
-bip8 = bip8.filter(r => r.reg > 10358142);
-bip8.sort((a, b) => a.loops - b.loops);
-bipgrr = bipgrr.filter(r => r.reg > 10358142);
-bipgrr.sort((a, b) => a.loops - b.loops);
-let lowestReg0 = bip28[bip28.length - 1].reg;
-let mostLoops = 0;
-let backupReg = [0, 0, 0, 0, 0, 0];
-let backupLoops = 0;
-//bip28.forEach((r, i) => {
-bipgrr.forEach((r, i) => {
-    run_program(r.loops + 10000, r.reg, true)
-    console.log(bipgrr.length - 1 - i);
-});
+// 11777564 (second answer)
 
 function run_program(max, reg0, check) {
     let registers = [reg0, 0, 0, 0, 0, 0];
     let loops = 0;
     let halted = false;
-    let max2 = 0;
 
-    if (check) {
-        registers = backupReg.slice(0);
-        registers[0] = reg0;
-    }
+    while (!halted && loops < max) {
+        const instruction = registers[ip];
+        if (instruction == 28 && registers[2] > 0) {
+            const reg = registers[5];
 
-    do {
-        const beforeReg = registers.slice(0);
-        const bip = registers[ip];
-        //console.log(`${bip} - ${program[bip].opcode} ${program[bip].a} ${program[bip].b} ${program[bip].c}`);
-        if (bip == 28) {
-            // 127181 too low
-            // 6911292 too low
-            // 10358142 too low
-            // 16777215 is wrong
-            // 16738346 is wrong
-            // 16711317 is wrong //(16777215 * 65899) & 16777215;
-            // Lowest Reg 0: 11153993 Loops: 2688935
-            // 11153993 is wrong
-            // 1105013081417 is wrong
-            //console.log(`loops=${loops} registers[5]=${registers[5]}`)
-            if (!check) {
-                const reg = registers[5];// //& 16777215;
-                const found = _.findWhere(bip28, { reg: reg });
-                if (!found) {
-                    bip28.push({
-                        reg: reg,
-                        loops: loops
-                    });
-                }
-                else{
-                    console.log('ooo');
-                }
-
-            }
-            //registers[0]=registers[5];
-            if (check && registers[5] == registers[0]) {
-                backupReg = registers.slice(0);
-                backupLoops = loops;
-            }
-
-        }
-        //console.log(`[${beforeReg.join(',')}]`);
-        if (registers[ip] == 8 && registers[3] <= 256) {
-            //console.log(`8 [${beforeReg.join(',')}]`);
-            // console.log(`${registers[3].toString(2)}`)
-            const reg = ((((registers[3] & 255) + registers[5]) & 16777215) * 65899) & 16777215;
-            //console.log(reg)
-            const found = _.findWhere(bip8, { reg: reg });
+            const found = arrInstruction28.indexOf(reg) > -1;
             if (!found) {
-                bip8.push({
-                    reg: reg,
-                    loops: loops
-                });
-            }
-            if (found) {
-                console.log('oo a repeat!')
+
+                arrInstruction28.push(reg);
+                console.log(arrInstruction28.length);
+            } else {
+                halted = true;
             }
         }
+
         registers = op(program[registers[ip]], registers);
         registers[ip]++;
-        // if (!check && registers[5] != largestReg5) {
-        //     largestReg5 = registers[5];
-        //     console.log(`largestReg5=${largestReg5} loops ${loops}`);
-        //     console.log(`${largestReg5.toString(2)}`)
-        // }
-
-        //if (false && loops > 172860175) {
-        if (loops > 1820) {
-            //if (registers[1] > registers[3]) {
-            //if (registers[5] == 16777215) {
-            if (!check) {
-                bipgrr.push({
-                    reg: registers[5],
-                    loops: loops
-                });
-                // if (!check && registers[5] > largestReg5) {
-                //     largestReg5 = registers[5];
-                //     console.log(`largestReg5=${largestReg5}`);
-                // }
-            }
-            console.log(`[${beforeReg.join(',')}]`);
-            console.log(`${bip} - ${program[bip].opcode} ${program[bip].a} ${program[bip].b} ${program[bip].c}`);
-            console.log(`[${registers.join(',')}]`);
-            // console.log('\n');
-        }
         loops++;
-        halted = !(registers[ip] < program.length)
-        if (halted) {
-            //console.log('loop')
-            //registers[ip]=0
-            //registers = backupReg;
-            //registers[5]++;
-            //registers[0]++;
-            //halted=false;
-        }
-        //if (registers[2] > max2) max2 = registers[2];
-
-    }
-    while (!halted && loops < max);
-    //console.log(`Max registers[5]=${registers[5]}`)
-    if (halted) {
-        // console.log(`[${registers.join(',')}]`);
-        // console.log(`reg0=${reg0}, loops=${loops}`)
-        // console.log(`Max registers[2]=${registers[2]}`)
-        if (check) {
-            if (loops > mostLoops) {
-                mostLoops = loops;
-                lowestReg0 = reg0;
-                console.log(`new mostLoops=${mostLoops} reg0=${reg0}`);
-            }
-        }
-        console.log('halt')
+        halted = halted || !(registers[ip] < program.length);
     }
 
     return halted;
-}
-console.log(`Lowest Reg 0: ${lowestReg0} Loops: ${mostLoops}`);
-
-function genReg() {
-
-    //     ni 5 16777215 5
-    // [202209,1,1,1,11,883939]
-
-
-    // [202209,1,1,1,11,883939]
-    // 11 - muli 5 65899 5
-    //const a = (16777215 * 65899) & 16777215;
-
-    const max = 20000;
-    let i = 0;
-    let reges = [];
-    while (i < max) {
-        const ireg = i * 65899;
-        const reg = ireg & 16777215;
-        if (reg > 10358142) {
-            const found = _.findWhere(reges, { reg: reg });
-            if (!found) {
-                reges.push({
-                    reg: reg
-                });
-            }
-        }
-        i++;
-    }
-    //reges = reges.filter(r => r.reg > 10358142);
-    //bipgrr.sort((a, b) => a.loops - b.loops);
-    console.log('reges ready ' + reges.length);
-    return reges;
 }
 
 function op(_in, _r) {
