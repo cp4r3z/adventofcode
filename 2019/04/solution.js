9204a5286507fc52672c60ee208a0252d3ce0023
charles <charles.parez@3ds.com>
2019-12-04 14:57:50 -0600

    Day 4 Solutions

--- 2019/04/solution.js
+++ 2019/04/solution.js
@@ -6,25 +6,13 @@ let passwords2 = [];
 // Quick and dirty
 for (let pw = range.min; pw <= range.max; pw++) {
     if (check(pw)) passwords.push(pw);
-    if (checkPart2(pw)) passwords2.push(pw);
+    if (check(pw,true)) passwords2.push(pw);
 }
 
 console.log(`Part 1: ${passwords.length} passwords`);
 console.log(`Part 2: ${passwords2.length} passwords`);
 
-function check(pw) {
-    if (pw < range.min || pw > range.max) return false;
-    const pwArray = Array.from(String(pw), Number);
-    if (pwArray.length !== 6) return false;
-    let doubleFound = false;
-    for (let i = 1; i < pwArray.length; i++) {
-        if (!doubleFound) doubleFound = pwArray[i - 1] === pwArray[i];
-        if (pwArray[i - 1] > pwArray[i]) return false;
-    }
-    return doubleFound;
-}
-
-function checkPart2(pw) {
+function check(pw,triplesNotAllowed) {
     //if (pw < range.min || pw > range.max) return false;
     const pwArray = Array.from(String(pw), Number);
     if (pwArray.length !== 6) return false;
@@ -32,12 +20,16 @@ function checkPart2(pw) {
     for (let i = 1; i < pwArray.length; i++) {
         if (pwArray[i - 1] > pwArray[i]) return false;
         if (!doubleFound) {
-            if (i === 1) {
-                doubleFound = pwArray[i - 1] === pwArray[i] && pwArray[i] !== pwArray[i + 1];
-            } else if (i == pwArray.length - 1) {
-                doubleFound = pwArray[i - 1] === pwArray[i] && pwArray[i] !== pwArray[i - 2];
+            if (triplesNotAllowed){
+                if (i === 1) {
+                    doubleFound = pwArray[i - 1] === pwArray[i] && pwArray[i] !== pwArray[i + 1];
+                } else if (i == pwArray.length - 1) {
+                    doubleFound = pwArray[i - 1] === pwArray[i] && pwArray[i] !== pwArray[i - 2];
+                } else {
+                    doubleFound = pwArray[i - 1] === pwArray[i] && pwArray[i] !== pwArray[i - 2] && pwArray[i] !== pwArray[i + 1];
+                }
             } else {
-                doubleFound = pwArray[i - 1] === pwArray[i] && pwArray[i] !== pwArray[i - 2] && pwArray[i] !== pwArray[i + 1];
+                doubleFound = pwArray[i - 1] === pwArray[i];
             }
         }
     }
