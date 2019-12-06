bc8b5e496b7e95d7d599bf98de1de5751483c65f
charles <charles.parez@3ds.com>
2019-12-06 09:18:20 -0600

    Day 5 Solutions

--- 2019/05/solution.js
+++ 2019/05/solution.js
@@ -7,12 +7,15 @@ const parser = require('./parser.mjs');
 
 // Parse Input
 
-const inputFileName = 'input.txt';
+let inputFileName = 'input.txt';
 const arrInput = parser.singleLine.commaSeparated.toIntArray(
     require('fs').readFileSync(`${__dirname}/${inputFileName}`, 'utf8')
 );
 
-run(1, 2, arrInput);
+console.log('\nPart 1...');
+run(1, arrInput);
+console.log('\nPart 2...');
+run(5, arrInput);
 
 // End Process (gracefully)
 process.exit(0);
