e8ebf116b97e1de54d7e9b399c974cef5ab3cb84
cp4r3z <cp4r3z@gmail.com>
2019-12-07 14:56:28 -0600

    Day 7 Solutions

--- 2019/07/solution.js
+++ 2019/07/solution.js
@@ -2,83 +2,91 @@
  * https://adventofcode.com/2019/day/7
  */
 
-const run = require('./intcode.mjs').run;
+const intCode = require('./intcode.mjs');
 const parser = require('./parser.mjs');
 
 // Parse Input
-
 let inputFileName = 'input.txt';
 const arrInput = parser.singleLine.commaSeparated.toIntArray(inputFileName);
 
-// Generate all possible phase combinations
+// Store the signals for all possible phase sequences
+let signalsFromSequences = [];
 
-let phaseComb = [];
+// Part 1 doesn't use a feedback loop
+let feedback = false;
+// Part 1 uses phases 0-4
+let validPhases = [0, 1, 2, 3, 4];
 
-let test = intArrayExcept([7, 1]);
+// Determine the signal for each sequence
+getSignalFromSequence([], 0);
 
-let test2 = intArrayExcept([]); //0-9
+let highestSignal = signalsFromSequences.sort((o1, o2) => o2.signal - o1.signal)[0];
+console.log(`Part 1: Thruster signal of ${highestSignal.signal} @ ${highestSignal.sequence}`);
 
-intArrayBuilder([],0);
-console.log('done building');
+// Part 2 resets the signalsFromSequences
+// Part 2 uses a feedback loop
+// Part 2 uses phases 5-9
+signalsFromSequences = [];
+feedback = true;
+validPhases = [5, 6, 7, 8, 9];
 
-// let testComb = [0, 1, 2, 3, 4];
-// let inputtest = 0;
-// testComb.forEach(ampPhase => {
-//     inputtest = run(ampPhase, inputtest, arrInput);
-// });
-// console.log('output:' + inputtest);
+// Determine the signal for each sequence
+getSignalFromSequence([], 0);
 
-// const outputs = phaseComb.map(p => {
-//     let input = 0;
-//     p.forEach(ampPhase => {
-//         input = run(ampPhase, input, arrInput);
-//     });
-//     return {
-//         sequence: p.join(','),
-//         signal: input // final output
-//     };
-// });
+highestSignal = signalsFromSequences.sort((o1, o2) => o2.signal - o1.signal)[0];
+console.log(`Part 2: Thruster signal of ${highestSignal.signal} @ ${highestSignal.sequence}`);
 
-const highestSignal = phaseComb.sort((o1, o2) => o2.signal - o1.signal)[0];
-console.log(`Part 1: Truster signal of ${highestSignal.signal} @ ${highestSignal.sequence}`);
+function getSignalFromSequence(inputStates, input) {
+    const registerPhases = inputStates.map(state => state.phase);
 
-function intArrayBuilder(array, input) {
-    //const arraySoFar = [...array];
+    if (registerPhases.length < 5) {
+        // Initial pass finds all phase sequence permutations
+        const nextPhases = validPhases.filter(p => !registerPhases.includes(p));
+        nextPhases.forEach(p => {
+            // Instantiate a new register
+            const states = [...inputStates];
+            const register = intCode(
+                { intCodes: arrInput } // Use the puzzle input array for initial intCodes
+            );
 
-    const nextPhases = intArrayExcept(array);
-    nextPhases.forEach(p => {
-        const nextArray = [...array];
-        const output = run(p, input, arrInput);
-        nextArray.push(p);
-        if (nextArray.length === 5) {
-            phaseComb.push({
-                sequence: nextArray.join(','),
-                signal: output
-            });
-            return;
-        } else {
-            intArrayBuilder(nextArray, output);
-        }
+            // Using the next phase, store it's initial output and state.
+            let output = register.run(p, input);
+            states.push(register.getState());
 
-    });
-}
-
-// Return 0-9 except those in integer array
-function intArrayExcept(intArray) {
-    return [...Array(10).keys()]
-        .filter(k => {
-            let valid = true;
-            intArray.forEach(i => {
-                valid = valid && k != i;
-            });
-            return valid;
+            // Continue recursion
+            getSignalFromSequence(states, output);
         });
-}
+    } else {
+        // All registers have been run once
+        if (!feedback) {
+            // Part 1
+            signalsFromSequences.push({
+                sequence: inputStates.map(state => state.phase).join(','),
+                signal: input
+            });
+        } else {
+            // Part 2
+            const registers = inputStates.map(state => intCode(state));
+            let registerIndex = 0;
+            let outputE = input;
+            let halt = false;
+            let output = input;
+            do {
+                output = registers[registerIndex].run(false, output);
+                if (output === false) halt = true;
+                if (registerIndex === 4) {
+                    outputE = output;
+                }
+                registerIndex = (registerIndex === 4 ? 0 : registerIndex + 1);
+            } while (!halt);
 
-// console.log('\nPart 1...');
-// const test1 = run(1, 1, arrInput);
-// console.log('\nPart 2...');
-// const test2 = run(5, 5, arrInput);
+            signalsFromSequences.push({
+                sequence: inputStates.map(state => state.phase).join(','),
+                signal: outputE
+            });
+        }
+    }
+}
 
 // End Process (gracefully)
 process.exit(0);
