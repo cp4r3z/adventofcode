5a337ed854a8ee4b8274e58af676b8d1759af862
cp4r3z <cp4r3z@gmail.com>
2019-12-14 16:13:23 -0600

    Day 14 Part 1 Working

--- 2019/14/solution.js
+++ 2019/14/solution.js
@@ -9,10 +9,44 @@ let inputFileName = 'input.txt';
 const arrInput = parser.multiLine.toStrArray(inputFileName);
 
 let reactions = [];
+let reserves = {
+    'ORE':0
+};
 
 reYields = /(.+) => (.+)/g;
 
-arrInput.forEach(s => {
+arrInput.forEach(fillReactions);
+
+const oneFuel = getOreRequirement('FUEL', 1);
+
+console.log(oneFuel);
+// recursive function 
+
+function getOreRequirement(requestedChem, requestedQty) {
+    const chemsTest = reactions.filter(r => r.out.chem === requestedChem);
+    if (chemsTest.length !== 1) console.error('Error: Must be exactly 1 way to create a chemical');
+    const chemical = reactions.filter(r => r.out.chem === requestedChem)[0];
+    //TODO: maybe check here for multiple reactions?
+
+
+    //todo: figure out qty of IN needed to make OUT requested in quantity
+
+    //requested / out < math.ceiling
+    const multipleOut = Math.ceil((requestedQty-reserves[requestedChem]) / chemical.out.qty);
+    const reserveOut = chemical.out.qty*multipleOut-requestedQty;
+    reserves[requestedChem]=reserves[requestedChem]+reserveOut;
+
+    if (chemical.ore !== null) { //chemical.in[0].chem==='ORE'
+        return chemical.ore * multipleOut;
+    }
+    const reduction = chemical.in.reduce((oreRequirement, reactant) => {
+        return oreRequirement + getOreRequirement(reactant.chem, reactant.qty * multipleOut);
+    }, 0);
+
+    return reduction;
+}
+
+function fillReactions(s) {
     const yields = reYields.exec(s);
     const reactantsString = yields[1]; //reagents
     const productString = yields[2];
@@ -26,16 +60,19 @@ arrInput.forEach(s => {
         out: product,
         ore
     });
-});
+    reserves[product.chem]=0;
+}
 
 function mapQuantityChemical(strQtyChem) {
     const reQtyChem = /(\d+) (\w+)/g;
     const qtyChem = reQtyChem.exec(strQtyChem);
     return {
-        qty: qtyChem[1],
+        qty: parseInt(qtyChem[1], 10),
         chem: qtyChem[2]
     };
 }
+
+//Part 1: 609232 is too high
 
 // End Process (gracefully)
 process.exit(0);
