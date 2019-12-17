19469d8f1e1049cfc7ed1c3a2ee760f2f5d1b2a1
cp4r3z <cp4r3z@gmail.com>
2019-12-17 07:56:53 -0600

    Day 16 WIP

--- 2019/16/solution.js
+++ 2019/16/solution.js
@@ -3,33 +3,127 @@
  */
 
 const parser = require('./parser.mjs');
+const fs = require('fs');
 
 // Parse Input
-let inputFileName = 'input.txt';
+let inputFileName = 'inputtest2.txt';
 const arrInput = parser.singleLine.notSeparated.toIntArray(inputFileName);
 
 // Part 1 
 
-let phasedInput = [...arrInput];
+// let phasedInput = [...arrInput];
+// phasedInput = phaseInput(phasedInput);
 
+// console.log(`Part 1: ${phasedInput.splice(0,8).join('')}`);
 
-for (let index = 0; index < 100; index++) {
-    let output = Array(phasedInput.length);
+// Part 2
 
-    for (let i = 0; i < phasedInput.length; i++) {
-        const phase = genPhase(phasedInput.length, i);
-        const appliedPhase = phasedInput.reduce((acc, digit, j) => acc + digit * phase[j], 0);
-        const str = appliedPhase.toString();
-        const num = str.charAt(str.length - 1);
-        const lastCharToInt = parseInt(num, 10);
-    
-        output[i] = lastCharToInt;
-    }
-    
-    phasedInput = [...output];
+// Figure out offset
+let part2Offset = arrInput.slice(0, 6).join('');
+part2Offset = Number(part2Offset);
+
+// Find array of length 8 with the message (after the input's been multiplied 10,000 times)
+let part2Message = Array(8);
+
+const arrInputMultipliedLength = arrInput.length * 10000;
+for (let index = 0; index < part2Message.length; index++) {
+    const digitIndex = (part2Offset + index) % arrInput.length;
+    part2Message[index] = arrInput[digitIndex];
 }
 
-console.log(`Part 1: ${phasedInput.join('')}`);
+console.log(Date());
+const phasetest1 = genPhase(arrInput.length * 10000, 1234).slice(1234, 1242);
+const phasetest2 = genPhase(arrInput.length * 10000, 1235).slice(1234, 1242);
+const phasetest3 = genPhase(arrInput.length * 10000, 1236).slice(1234, 1242);
+console.log(Date());
+
+let phasedInput2 = Array(arrInput.length);
+phasedInput2.fill(1);
+
+phasedInput2 = phaseInput(phasedInput2);
+
+
+/*
+let phasedInput2 = [];
+for (let index = 0; index < 10000; index++) {
+    phasedInput2 = phasedInput2.concat(arrInput);
+}
+console.log(Date());
+phasedInput2 = phaseInput(phasedInput2);
+console.log(Date());
+
+let offset = phasedInput2.splice(0,7).join('');
+offset = parseInt(offset,10); // TEST this
+console.log(Date());
+console.log(offset);
+*/
+
+
+// for (let index = 0; index < 100; index++) {
+//     let output = Array(refInputArray.length);
+
+//     for (let i = 0; i < refInputArray.length; i++) {
+//         const phase = genPhase(refInputArray.length, i);
+//         const appliedPhase = refInputArray.reduce((acc, digit, j) => acc + digit * phase[j], 0);
+//         const str = appliedPhase.toString();
+//         const num = str.charAt(str.length - 1);
+//         const lastCharToInt = parseInt(num, 10);
+
+//         output[i] = lastCharToInt;
+//     }
+
+//     refInputArray = [...output];
+// }
+
+
+//fs.appendFileSync('phases.txt', 'data to append');
+
+//offset = 0;
+
+//console.log(`Part 1: ${phasedInput2.splice(offset,8).join('')}`);
+
+fs.unlinkSync('phases.txt');
+for (let i = 0; i < 110; i++) {
+    const phase = genPhase(arrInput.length, i);
+    const part2 = phase.join(',').replace(/,0/gi, ', 0').replace(/,1/gi, ', 1');
+    fs.appendFileSync('phases.txt', part2 + '\n', 'utf8');
+}
+
+
+//fs.unlinkSync('mods.txt');
+let mods = [];
+for (let i = 0; i < 110; i++) {
+    mods.push(i)
+}
+fs.appendFileSync('mods.txt', mods, 'utf8');
+
+function phaseInput(refInputArray) {
+
+
+
+    for (let index = 0; index < 100; index++) {
+        let output = Array(refInputArray.length);
+
+        for (let i = 0; i < refInputArray.length; i++) {
+            const phase = genPhase(refInputArray.length, i);
+            const appliedPhase = refInputArray.reduce((acc, digit, j) => acc + digit * phase[j], 0);
+            const str = appliedPhase.toString();
+            const num = str.charAt(str.length - 1);
+            const lastCharToInt = parseInt(num, 10);
+
+            output[i] = lastCharToInt;
+        }
+
+        refInputArray = [...output];
+
+    }
+
+    return refInputArray; // I don't know why this isn't changing the referenced array.
+}
+
+
+
+
 
 // Phase Generator
 
