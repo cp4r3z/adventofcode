0a863e5e7d55818bbaaa64ebdaef80fcf938f7bb
cp4r3z <cp4r3z@gmail.com>
2020-12-05 07:52:35 -0600

    Y2020D05 - Cleanup

--- 2020/05/solution.mjs
+++ 2020/05/solution.mjs
@@ -10,19 +10,9 @@ const arrInput = multiLine.toStrArray(inputFilePath);
 
 // Decode the seat position by converting to binary. Maybe this is cheating?
 const decodedSeats = arrInput.map(line => {
-    const rowFandB = line.substring(0, 7);
-    const rowBin = rowFandB.replace(/F/g, '0').replace(/B/g, '1');
-    const rowDec = parseInt(rowBin, 2);
-    const colLandR = line.substring(7, 10);
-    const colBin = colLandR.replace(/L/g, '0').replace(/R/g, '1');
-    const colDec = parseInt(colBin, 2);
-    const seatID = 8 * rowDec + colDec;
-    return {
-        line,
-        seatID,
-        row: rowDec,
-        col: colDec
-    };
+    const lineBin = line.replace(/F/g, '0').replace(/B/g, '1').replace(/L/g, '0').replace(/R/g, '1');
+    const lineDec = parseInt(lineBin, 2);
+    return { line, seatID: lineDec };
 });
 
 const sortedSeats = decodedSeats.sort((seatA, seatB) => seatB.seatID - seatA.seatID);
@@ -33,14 +23,10 @@ console.log(`Year 2020 Day 05 Part 1 Solution: ${highestSeatID}`);
 // Find a "missing" seat ID
 let yourSeat;
 for (let index = 0; index < sortedSeats.length; index++) {
-    const seat = sortedSeats[index];
-    if (!(sortedSeats[index + 1].seatID === seat.seatID - 1)) {
-        yourSeat = seat.seatID - 1;
+    if (sortedSeats[index + 1].seatID !== sortedSeats[index].seatID - 1) {
+        yourSeat = sortedSeats[index].seatID - 1;
         break;
     }
 }
 
 console.log(`Year 2020 Day 05 Part 2 Solution: ${yourSeat}`);
-
-// End Process (gracefully)
-process.exit(0);
