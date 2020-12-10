37e6b5ab00819bdd9591f8bac6cbd680ac776e10
charles <charles.parez@3ds.com>
2020-12-10 10:40:28 -0600

    Y2020D10 - Solutions

--- 2020/10/solution.mjs
+++ 2020/10/solution.mjs
@@ -27,5 +27,40 @@ for (let i = 1; i < sortedAdapters.length; i++) {
 
 console.log(`\nYear 2020 Day 10 Part 1 Solution: ${diff1 * diff3}`);
 
+const joltageToIndex = new Map();
+sortedAdapters.forEach((adapterJoltage, index) => {
+    joltageToIndex[adapterJoltage] = index;
+});
+
+// Add the voltage key to this map only when you're sure that all children joltages are known.
+const arrangementCache = new Map();
+
+function findArrangements(joltage) {
+    if (arrangementCache.has(joltage)){
+        return arrangementCache.get(joltage);
+    }
+    const nextJoltages = findChildren(joltage);
+    if (nextJoltages.length===0) return 1;
+    const arrangements = nextJoltages.reduce((acc, j) => acc+ findArrangements(j), 0);
+
+    arrangementCache.set(joltage, arrangements);
+    // return the number of arrangements
+    return arrangements;
+}
+
+const arrangementCount = findArrangements(0);
+
+console.log(`\nYear 2020 Day 10 Part 1 Solution: ${arrangementCount}`);
+
+function findChildren(fromJoltage) {
+    const validJoltages = [];
+    //TODO: Is there a better way to do this?
+    if (sortedAdapters.includes(fromJoltage + 1)) validJoltages.push(fromJoltage + 1);
+    if (sortedAdapters.includes(fromJoltage + 2)) validJoltages.push(fromJoltage + 2);
+    if (sortedAdapters.includes(fromJoltage + 3)) validJoltages.push(fromJoltage + 3);
+    return validJoltages;
+}
+
+
 // End Process (gracefully)
 process.exit(0);
