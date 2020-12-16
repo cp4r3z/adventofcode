7ffd10b6ac726e1cca6e26a0b07302095c2c7924
cp4r3z <cp4r3z@gmail.com>
2020-12-15 18:44:16 -0600

    Y2020D14 - Part 2 WIP

--- 2020/14/solution.mjs
+++ 2020/14/solution.mjs
@@ -5,7 +5,7 @@
 import { multiLine } from '../../common/parser.mjs';
 
 // Parse Input
-const inputFilePath = new URL('./input.txt', import.meta.url);
+const inputFilePath = new URL('./tinput.txt', import.meta.url);
 const arrInput = multiLine.toStrArray(inputFilePath);
 
 const program = [];
@@ -39,30 +39,63 @@ for (let programIndex = 0; programIndex < program.length; programIndex++) {
     instructionSetRunning.instructions.forEach(processInstruction);
 }
 
-const testtest = convertbin36ArrayToDec([0, 0, 0, 1, 0, 1, 1]);
-
-
-const test829902099 = convertbin36ArrayToDec(convertDectoBin36Array(829902099));
-
 let total = 0;
 for (const address in addresses) {
 
     const addressValue = addresses[address];
 
     total += convertbin36ArrayToDec(addressValue);
+}
 
-    // for (const i of addressValue) {
-    //     if (i === 1) total++;
-    // }
+console.log(`Year 2020 Day 14 Part 1 Solution: ${total}`);
+
+
+// part2 . ..
+
+/// last mask
+
+let part2Total = 0;
+
+function subTotal(iProgram) {
+    let _subTotal = 0;
+    const subMask = program[iProgram].mask;
+    program[iProgram].instructions.forEach(instruction => {
+        const value = instruction.value;
+        const maskedValue = memPlusMask(subMask, convertDectoBin36Array(instruction.mem)); // mem not value
+        const numFloatX = maskedValue.filter(b => b === 'X').length;
+        _subTotal+= value* Math.pow(2,numFloatX);
+    });
+    return _subTotal;
+}
+
+part2Total += subTotal(program.length - 1);
+
+// Working backwards from the 2nd to last mask...
+for (let iMask = program.length - 2; iMask > -1; iMask--) {
+    const mask = program[iMask].mask;
+
+    // now go up all the later masks to see if it'll be overwritten. adjust mask accordingly.
+
+
+    continue;
+
 
 }
 
 
-////// 5896 is too low
-/// 11901186698673 is too low
-
 function processInstruction(instruction) {
     addresses[instruction.mem] = valuePlusMask(maskRunning, convertDectoBin36Array(instruction.value));
+}
+
+function memPlusMask(mask, mem) {
+    let result = getBits();
+    for (let i = 0; i < mem.length; i++) {
+        // per part2 rules
+        if (mask[i] === '0') result[i] = parseInt(mem[i], 10);
+        else if (mask[i] === '1') result[i] = 1;
+        else if (mask[i] === 'X') result[i] = 'X';
+    }
+    return result;
 }
 
 function valuePlusMask(mask, value) {
@@ -105,8 +138,6 @@ function convertbin36ArrayToDec(bin) {
     return dec;
 }
 
-
-//console.log(`Year 2020 Day 13 Part 1 Solution: ${part1}`);
 
 // End Process (gracefully)
 process.exit(0);
