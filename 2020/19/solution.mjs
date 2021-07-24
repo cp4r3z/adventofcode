db1a44b7e05decff81c58fc3de4050be815d96b5
cp4r3z <cp4r3z@gmail.com>
2021-07-23 20:12:22 -0500

    Y2020D19 - Recursion not working

--- 2020/19/solution.mjs
+++ 2020/19/solution.mjs
@@ -22,6 +22,54 @@ class Rule {
         this.SubRules = arrSubs;
         this.IsLeaf = arrSubs.length === 0;
     }
+
+    isValid(str) {
+        if (this.IsLeaf) {
+            let newStr = str.split('');
+            const testChar = newStr.shift();
+            newStr = newStr.join(''); // This isn't super efficient.
+            const valid = testChar === this.RuleString; // last step in recursion
+            return valid ? newStr : str; // wish we could return false....
+        }
+
+        // Not a leaf
+        let workStr = str;
+        for (let si = 0; si < this.SubRules.length; si++) {
+            const workStrBackup = workStr;
+            const rules = this.SubRules[si];
+            //const workStrLengthPrev = workStr.length;
+            for (let ri = 0; ri < rules.length; ri++) {
+                const rule = rules[ri];
+                //const workStrLengthPrev = workStr.length;
+                workStr = rule.isValid(workStr);
+                
+            }
+            if (workStr.length === str.length) {
+                // didn't do anything
+                break;
+            }
+        }
+
+        return workStr;
+
+
+        //return workStr.length === 0; // ?
+
+        // let workStr = str;
+
+        // const subsValid = this.SubRules.forEach(rules=>{       
+        //     let keepGoing = true; 
+        //     rules.forEach(rule=>{
+        //         if (!keepGoing) return;
+        //         workStr = rule.isValid(workStr);
+        //         keepGoing = !!workStr; // still chars left
+        //     });
+        //     // let keepGoing = true;
+        //     // while ()
+        // });
+
+        // return workStr.length===0;
+    }
 }
 
 const rules = new Map();
@@ -84,6 +132,10 @@ rules.forEach(rule => {
     rule.SubRules = ptrSubRules;
 });
 
-
+const test0 = rules.get(0).isValid('ababbb'); // good
+const test1 = rules.get(0).isValid('bababa');
+const test2 = rules.get(0).isValid('abbbab'); // good
+const test3 = rules.get(0).isValid('aaabbb');
+const test4 = rules.get(0).isValid('aaaabbb');
 
 console.log(`Year 2020 Day 19 Part 1 Solution: ${part1}`);
