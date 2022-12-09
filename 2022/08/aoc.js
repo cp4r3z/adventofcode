ed4957c1344d91c83836bee9700268938f5ae104
PAREZ Charles <charles.parez@3ds.com>
2022-12-09 13:54:26 -0600

    Y2022D08 - Solutions

--- 2022/08/aoc.js
+++ 2022/08/aoc.js
@@ -26,21 +26,7 @@ class Tree {
         this.vis = false;
         this.edge = false;
     }
-    Look(delta) {
-        var atx = this.x + delta.x;
-        var aty = this.y + delta.y;
-        var at = patch.get(atx, aty);
-        if (atx === 1 && aty === 2) {
-            console.log('hi');
-        }
-        if (at.h > this.h) {
-            at.vis = true;
-        }
-        if (at.edge) {
-            return;
-        }
-        at.Look(delta);
-    }
+
     LookAround() {
         const deltas = [
             { x: 1, y: 0 },
@@ -65,9 +51,37 @@ class Tree {
             }
         });
     }
+
+    ViewingDistance() {
+        if (this.edge) { return 0; }
+        const deltas = [
+            { x: 1, y: 0 },
+            { x: -1, y: 0 },
+            { x: 0, y: 1 },
+            { x: 0, y: -1 }
+        ];
+        const distances = deltas.map(d => {
+            if (this.vis) { return; }
+            var atx = this.x;
+            var aty = this.y;
+            var looking = true;
+            var distance = 0;
+            while (looking) {
+                distance++;
+                atx += d.x;
+                aty += d.y;
+                var at = patch.get(atx, aty);
+                if (at.h >= this.h || at.edge) {
+                    looking = false;
+                }
+            }
+            return distance;
+        });
+        return distances.reduce((a, b) => a *= b, 1);
+    }
 }
 
-var patch = new Patch();
+// Parse Input
 
 var lines = input
     .split("\n")
@@ -78,18 +92,30 @@ var lines = input
 
 var dim = lines.length;
 
-for (let y = 0; y < dim; y++) {
-    const col = lines[y];
-    for (let x = 0; x < col.length; x++) {
-        const h = col[x];
-        var tree = new Tree(x, y, h);
-        if (x === 0 || y === 0 || x === dim - 1 || y === dim - 1) {
-            tree.edge = true;
-            tree.vis = true;
+var patch = null;
+
+function FillPatch() {
+    patch = new Patch();
+
+    for (let y = 0; y < dim; y++) {
+        const col = lines[y];
+        for (let x = 0; x < col.length; x++) {
+            const h = col[x];
+            var tree = new Tree(x, y, h);
+            if (x === 0 || y === 0 || x === dim - 1 || y === dim - 1) {
+                tree.edge = true;
+                tree.vis = true;
+            }
+            patch.set(x, y, tree);
         }
-        patch.set(x, y, tree);
     }
 }
+
+const ResetPatch = () => patch.forEach(tree => tree.vis = false);
+
+// Part 1
+
+FillPatch();
 
 for (let y = 1; y < dim - 1; y++) {
     for (let x = 1; x < dim - 1; x++) {
@@ -100,5 +126,21 @@ for (let y = 1; y < dim - 1; y++) {
 
 const part1 = Array.from(patch.values()).filter(t => t.vis).length;
 
+// Part 2
+
+let mostVisible = 0;
+for (let y = 0; y < dim; y++) {
+    for (let x = 0; x < dim; x++) {
+        ResetPatch();
+        var tree = patch.get(x, y);
+        var vd = tree.ViewingDistance();
+        if (vd > mostVisible) {
+            mostVisible = vd;
+        }
+    }
+}
+
+const part2 = mostVisible;
+
 document.getElementById("part1-result").innerText = `${part1}`;
-//document.getElementById("part2-result").innerText = `${part2}`;
+document.getElementById("part2-result").innerText = `${part2}`;
