2fdc6d60fdd6166bcbdb1353980904321bd84e36
PAREZ Charles <charles.parez@3ds.com>
2022-12-10 16:05:40 -0600

    Y2022D09 - Solutions

--- 2022/09/aoc.js
+++ 2022/09/aoc.js
@@ -6,50 +6,49 @@
  *   1...
  */
 
-var GridH = aoc.grid2D({}, ".");
-var H = { x: 0, y: 0 };
-
-const StepH = (dx, dy) => {
-    H.x += dx;
-    H.y += dy;
-    GridH.set(H.x, H.y, "#");    
-};
-StepH(0,0);
-
-// TODO: DRY
-var GridT = aoc.grid2D({});
-var T = { x: 0, y: 0 };
-
-const StepT = (dx, dy) => {
-    T.x += dx;
-    T.y += dy;
-    GridT.set(T.x, T.y, "#");
-};
-StepT(0,0);
-
-const UpdateT = () => {
-    let dx = H.x - T.x;
-    let dy = H.y - T.y;
-
-    // Only move if stretched
-    if (Math.abs(dx) < 2 && Math.abs(dy) < 2) { return; }
-
-    // Only move max of 1 in x and y
-    if (dx > 1) {
-        dx = 1;
-    }
-    if (dy > 1) {
-        dy = 1;
-    }
-    if (dx < -1) {
-        dx = -1;
-    }
-    if (dy < -1) {
-        dy = -1;
+class RopePart {
+    constructor(parent) {
+        this.grid = aoc.grid2D({}, ".");
+        this.pos = { x: 0, y: 0 };
+        this.next = null;
+        if (parent) {
+            this.parent = parent;
+            parent.next = this;
+        }
+        this.Step(0, 0);
     }
 
-    StepT(dx, dy);
-};
+    Step(dx, dy) {
+        this.pos.x += dx;
+        this.pos.y += dy;
+        this.grid.set(this.pos.x, this.pos.y, "#");
+        this.next?.Update();
+    }
+
+    Update() {
+        let dx = this.parent.pos.x - this.pos.x;
+        let dy = this.parent.pos.y - this.pos.y;
+
+        // Only move if stretched
+        if (Math.abs(dx) < 2 && Math.abs(dy) < 2) { return; }
+
+        // Only move max of 1 in x and y
+        if (dx > 1) {
+            dx = 1;
+        }
+        if (dy > 1) {
+            dy = 1;
+        }
+        if (dx < -1) {
+            dx = -1;
+        }
+        if (dy < -1) {
+            dy = -1;
+        }
+
+        this.Step(dx, dy);
+    };
+}
 
 // Parse Input
 
@@ -71,23 +70,37 @@ var moves = aoc.input
         return { dx, dy, steps };
     });
 
-moves.forEach(move => {
-    for (let step = 0; step < move.steps; step++) {
-        StepH(move.dx, move.dy);
-        //GridH.print();
-        UpdateT();
-        //console.log('');
-        //GridT.print();
-    }
-});
+function FindSolution(length) {
+    var rope = [];
+    const ropeLength = length;
 
-var part1 = 0;
-for (const key in GridT.dump()) {
-    //console.log(`${property}: ${object[property]}`);
-    if (GridT.dump()[key].value==="#"){
-        part1++;
+    for (let i = 0; i < ropeLength; i++) {
+        var parent = i > 0 ? rope[i - 1] : null;
+        var part = new RopePart(parent);
+        rope.push(part);
     }
-  }
+
+    var head = rope[0];
+    var tail = rope[ropeLength - 1];
+
+    moves.forEach(move => {
+        for (let step = 0; step < move.steps; step++) {
+            head.Step(move.dx, move.dy);
+        }
+    });
+
+    var solution = 0;
+    for (const key in tail.grid.dump()) {
+        if (tail.grid.dump()[key].value === "#") {
+            solution++;
+        }
+    }
+
+    return solution;
+}
+
+const part1 = FindSolution(2);
+const part2 = FindSolution(10);
 
 document.getElementById("part1-result").innerText = `${part1}`;
-//document.getElementById("part2-result").innerText = `${part2}`;
+document.getElementById("part2-result").innerText = `${part2}`;
